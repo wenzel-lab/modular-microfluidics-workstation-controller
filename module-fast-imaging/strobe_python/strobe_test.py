ab29ec3457c2daf1a15b3fcd81dd4eaae7e15ed2
Christie Nel <christie.nel@gmail.com>
2019-10-04 22:48:37 +0100

    SPI strobe interface working.

---
+++ module-fast-imaging/strobe_python/strobe_test.py
@@ -0,0 +1,27 @@
+import time
+import spidev
+
+# We only have SPI bus 0 available to us on the Pi
+bus = 0
+
+#Device is the chip select pin. Set to 0 or 1, depending on the connections
+device = 0
+
+# Enable SPI
+spi = spidev.SpiDev()
+
+# Open a connection to a specific bus and device (chip select pin)
+spi.open(bus, device)
+
+# Set SPI speed and mode
+spi.max_speed_hz = 500000
+spi.mode = 2
+
+msg = [3, 3, 1]
+spi.xfer2(msg)
+
+time.sleep(1)
+
+msg = [3, 3, 0]
+spi.xfer2(msg)
+
