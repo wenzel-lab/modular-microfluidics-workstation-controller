9bdf6481e7e2aa61caea5cecc76f928d620ce6da
Christie Nel <christie.nel@gmail.com>
2019-10-05 22:05:29 +0100

    Created basic packet routines.

--- module-fast-imaging/strobe_python/strobe_test.py
+++ module-fast-imaging/strobe_python/strobe_test.py
@@ -1,33 +1,42 @@
 import time
 import spidev
 
-# We only have SPI bus 0 available to us on the Pi
-bus = 0
+def strobe_packet_test():
+  msg = [7, 7, 7]
+  spi.xfer2( msg )
 
-#Device is the chip select pin. Set to 0 or 1, depending on the connections
-device = 0
+  time.sleep( 1 )
 
-# Enable SPI
+  msg = [7, 7, 7, 2, 5, 3, 1, 245]
+  msg = [2, 7, 7, 7, 7, 2, 5, 3, 1, 245]
+  spi.xfer2( msg )
+
+  time.sleep( 1 )
+
+  msg = [2, 5, 3, 0, 246]
+  spi.xfer2( msg )
+
+def strobe_packet_write( spi, type, data ):
+  msg = [2, len(data)+4, type] + data
+  msg = bytearray( msg )
+  checksum = ( -( sum( msg ) & 0xFF ) ) & 0xFF
+  msg.append( checksum )
+  spi.xfer2( msg )
+
+def spi_init( spi, bus, device, mode, speed_hz ):
+  spi.open( bus, device )
+  spi.mode = mode
+  spi.max_speed_hz = speed_hz
+  #spi.no_cs = False
+  #spi.cshigh = False
+
 spi = spidev.SpiDev()
+spi_init( spi, 0, 0, 2, 500000 )
 
-# Open a connection to a specific bus and device (chip select pin)
-spi.open(bus, device)
-
-# Set SPI speed and mode
-spi.max_speed_hz = 500000
-spi.mode = 2
-
-msg = [7, 7, 7]
-spi.xfer2(msg)
-
+strobe_packet_write( spi, 3, [1] )
 time.sleep(1)
+strobe_packet_write( spi, 3, [0] )
 
-#msg = [7, 7, 7, 2, 5, 3, 1, 245]
-msg = [2, 7, 7, 7, 7, 2, 5, 3, 1, 245]
-spi.xfer2(msg)
+#spi.readbytes(n)
 
-time.sleep(1)
-
-msg = [2, 5, 3, 0, 246]
-spi.xfer2(msg)
-
+spi.close()
