71e419cc960f3678ef77c4f2d68e960326ad7c08
Christie Nel <christie.nel@gmail.com>
2019-10-08 12:14:44 +0100

    Compensating for difference between frame time and shutter time.

--- module-fast-imaging/strobe_python/cam_test.py
+++ module-fast-imaging/strobe_python/cam_test.py
@@ -16,10 +16,6 @@ def spi_init( bus, device, mode, speed_hz ):
 
 spi = spi_init( 0, 0, 2, 500000 )
 strobe = PiStrobe( spi )
-strobe.set_timing( 100000, 4000000 )
-strobe.set_enable( True )
-#strobe.set_enable( False )
-#strobe.set_hold( True )
 
 camera = PiCamera(
     #resolution=
@@ -32,7 +28,7 @@ camera.awb_gains = ( Fraction( 1, 1 ), Fraction ( 1, 1 ) )
 camera.exposure_mode = 'off'
 camera.awb_mode = 'auto'
 #camera.resolution = ( 0, 0 )
-camera.framerate = 13
+camera.framerate = 50
 #camera.annotate_text = 'Test'
 #camera.brightness = 70
 #camera.contrast = 50
@@ -40,12 +36,27 @@ camera.shutter_speed = 64000
 camera.iso = 800
 camera.exposure_mode = 'off'
 
+# Inter-frame period in microseconds
+frame_rate_period_us = int( 1000000 / float( camera.framerate ) )
+
+# Dead time after frame, before sampling next frame
+strobe_pre_wait_us = frame_rate_period_us - camera.shutter_speed
+
+# How long the strobe is set to wait before triggering
+strobe_wait_ns = 100 + ( 1000 * strobe_pre_wait_us )
+
+print( 'Wait_ns {}'.format( strobe_wait_ns ) )
+
+strobe.set_timing( strobe_wait_ns, 2000000 )
+strobe.set_enable( True )
+
 camera.start_preview()
-#sleep(2)
-input()
+sleep(2)
+#input()
 #camera.capture('capture.jpg')
 #camera.start_recording('')
 camera.stop_preview()
 
+strobe.set_enable( False )
 camera.close()
 spi.close()
