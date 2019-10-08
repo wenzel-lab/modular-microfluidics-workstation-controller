042a0d2b107d1e5abbbf4aa29c3039ae1077f84b
Christie Nel <christie.nel@gmail.com>
2019-10-08 22:57:24 +0100

    Added strobe camera class.  Attempts to calculate camera timing.

--- module-fast-imaging/strobe_python/cam_test.py
+++ module-fast-imaging/strobe_python/cam_test.py
@@ -1,8 +1,9 @@
 import spidev
-from pistrobe import PiStrobe
-from picamera import PiCamera
+#from pistrobe import PiStrobe
+#from picamera import PiCamera
+from pistrobecam import PiStrobeCam
 from time import sleep
-from fractions import Fraction
+#from fractions import Fraction
 
 
 def spi_init( bus, device, mode, speed_hz ):
@@ -14,49 +15,20 @@ def spi_init( bus, device, mode, speed_hz ):
   #spi.cshigh = False
   return spi
 
-spi = spi_init( 0, 0, 2, 500000 )
-strobe = PiStrobe( spi )
+spi = spi_init( 0, 0, 2, 125000 )
+strobe_cam = PiStrobeCam( spi )
+strobe_cam.set_timing( 10000, 10000000 )
 
-camera = PiCamera(
-    #resolution=
-    framerate=Fraction(1, 1),
-#    sensor_mode=3
-    )
-
-camera.awb_gains = ( Fraction( 1, 1 ), Fraction ( 1, 1 ) )
-#camera.analog_gain = 100
-camera.exposure_mode = 'off'
-camera.awb_mode = 'auto'
-#camera.resolution = ( 0, 0 )
-camera.framerate = 50
-#camera.annotate_text = 'Test'
-#camera.brightness = 70
-#camera.contrast = 50
-camera.shutter_speed = 64000
-camera.iso = 800
-camera.exposure_mode = 'off'
-
-# Inter-frame period in microseconds
-frame_rate_period_us = int( 1000000 / float( camera.framerate ) )
-
-# Dead time after frame, before sampling next frame
-strobe_pre_wait_us = frame_rate_period_us - camera.shutter_speed
-
-# How long the strobe is set to wait before triggering
-strobe_wait_ns = 100 + ( 1000 * strobe_pre_wait_us )
-
-print( 'Wait_ns {}'.format( strobe_wait_ns ) )
-
-strobe.set_timing( strobe_wait_ns, 2000000 )
-strobe.set_enable( True )
-
-camera.start_preview()
+#strobe_cam.camera.zoom = 1
+strobe_cam.camera.start_preview()
 sleep(2)
 #input()
 #camera.capture('capture.jpg')
 #camera.start_recording('')
-camera.stop_preview()
+strobe_cam.camera.stop_preview()
+#strobe_cam.close()
+#sleep(2)
 
-strobe.set_enable( False )
-camera.close()
+strobe_cam.strobe.set_enable( False )
+strobe_cam.close()
 spi.close()
---
+++ module-fast-imaging/strobe_python/pistrobecam.py
@@ -0,0 +1,49 @@
+from pistrobe import PiStrobe
+from picamera import PiCamera
+
+class PiStrobeCam:
+    def __init__( self, spi ):
+        self.strobe = PiStrobe( spi )
+        self.camera = PiCamera(
+            #resolution=
+            #framerate=Fraction(1, 1),
+            #framerate = 50,
+            #sensor_mode=3
+        )
+        self.camera.awb_mode = 'auto'
+        self.camera.exposure_mode = 'off'
+        #self.camera.resolution = ( 0, 0 )
+        self.camera.iso = 800
+    
+    def set_timing( self, strobe_wait_padding_ns, strobe_period_ns ):
+        camera_read_time_us = 0
+        shutter_padding_us = 100000
+        self.camera.shutter_speed = int( ( ( strobe_period_ns + strobe_wait_padding_ns ) / 1000 ) + shutter_padding_us )
+#        self.camera.shutter_speed += camera_read_time_us
+        framerate = 1000000 / ( self.camera.shutter_speed + camera_read_time_us )
+        if ( framerate > 60 ):
+            framerate = 60
+        self.camera.framerate = framerate
+        
+        # Inter-frame period in microseconds
+        frame_rate_period_us = int( 1000000 / float( self.camera.framerate ) )
+        
+        # Dead time after frame, before sampling next frame
+        strobe_pre_wait_us = frame_rate_period_us - self.camera.shutter_speed
+        
+        # How long the strobe is set to wait before triggering
+        strobe_wait_ns = strobe_wait_padding_ns + ( 1000 * strobe_pre_wait_us )
+        
+        #strobe_wait_ns = strobe_wait_padding_ns
+        #strobe_period_ns += ( strobe_pre_wait_us * 1000 ) + 3000000
+        
+        print( 'wait {}, strobe {}, framerate {}, frametime {}, shutter {}'.format( strobe_wait_ns, strobe_period_ns, int( self.camera.framerate ), frame_rate_period_us, int( self.camera.shutter_speed ) ) )
+        
+        self.strobe.set_timing( strobe_wait_ns, strobe_period_ns )
+        self.strobe.set_enable( True )
+        
+    def close( self ):
+        #self.strobe.set_enable( False )
+        #self.strobe.set_hold( False )
+        self.camera.close()
+        
