0cfc8a68c13022e673e1cd1d0f726d8548bd7d7b
Christie Nel <christie.nel@gmail.com>
2020-02-24 17:57:16 +0000

    Removed pull-ups on SDI and CLK.  Problem caused by soldering and pull-ups were probably fine.

--- module-sample-holders/sample_holder_pic/mcc_generated_files/pin_manager.c
+++ module-sample-holders/sample_holder_pic/mcc_generated_files/pin_manager.c
@@ -80,7 +80,7 @@ void PIN_MANAGER_Initialize (void)
     CNPDA = 0x0000;
     CNPDB = 0x0000;
     CNPUA = 0x0000;
-    CNPUB = 0x07F7;
+    CNPUB = 0x07C7;
 
     /****************************************************************************
      * Setting the Open Drain SFR(s)
@@ -101,20 +101,20 @@ void PIN_MANAGER_Initialize (void)
     __builtin_write_RPCON(0x0000); // unlock PPS
 
     RPINR22bits.SCK2R = 0x0021;    //RB1->SPI2:SCK2OUT
-    RPOR0bits.RP33R = 0x0009;    //RB1->SPI2:SCK2OUT
-    RPOR7bits.RP46R = 0x0014;    //RB14->SCCP6:OCM6
-    RPOR7bits.RP47R = 0x0001;    //RB15->UART1:U1TX
-    RPINR21bits.SS1R = 0x0020;    //RB0->SPI1:SS1
-    RPINR20bits.SCK1R = 0x0025;    //RB5->SPI1:SCK1IN
-    RPOR1bits.RP34R = 0x0008;    //RB2->SPI2:SDO2
+    RPINR6bits.TCKI4R = 0x002A;    //RB10->SCCP4:TCKI4
+    RPINR5bits.ICM3R = 0x002A;    //RB10->SCCP3:ICM3
+    RPOR6bits.RP44R = 0x0010;    //RB12->SCCP2:OCM2
     RPOR3bits.RP39R = 0x000A;    //RB7->SPI2:SS2OUT
     RPOR1bits.RP35R = 0x0005;    //RB3->SPI1:SDO1
-    RPINR6bits.TCKI4R = 0x002A;    //RB10->SCCP4:TCKI4
+    RPINR20bits.SCK1R = 0x0025;    //RB5->SPI1:SCK1IN
+    RPOR7bits.RP47R = 0x0001;    //RB15->UART1:U1TX
+    RPINR21bits.SS1R = 0x0020;    //RB0->SPI1:SS1
     RPINR22bits.SDI2R = 0x0026;    //RB6->SPI2:SDI2
-    RPOR6bits.RP44R = 0x0010;    //RB12->SCCP2:OCM2
-    RPINR5bits.ICM3R = 0x002A;    //RB10->SCCP3:ICM3
-    RPINR20bits.SDI1R = 0x0024;    //RB4->SPI1:SDI1
     RPOR5bits.RP43R = 0x000F;    //RB11->SCCP1:OCM1
+    RPOR0bits.RP33R = 0x0009;    //RB1->SPI2:SCK2OUT
+    RPOR7bits.RP46R = 0x0014;    //RB14->SCCP6:OCM6
+    RPOR1bits.RP34R = 0x0008;    //RB2->SPI2:SDO2
+    RPINR20bits.SDI1R = 0x0024;    //RB4->SPI1:SDI1
 
     __builtin_write_RPCON(0x0800); // lock PPS
 
