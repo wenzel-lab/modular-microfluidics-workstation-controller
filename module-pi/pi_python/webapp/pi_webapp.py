97d7f929d500a25311b4f10ce2985306dfe533de
Christie Nel <christie.nel@gmail.com>
2020-05-04 18:22:37 +0100

    Automatic updates test.

--- module-pi/pi_python/webapp/pi_webapp.py
+++ module-pi/pi_python/webapp/pi_webapp.py
@@ -4,19 +4,17 @@ from flask import Flask, flash, render_template, request, redirect
 
 class strobe_data_class:
   hold = 0
+  num = 0
+
+strobe_data = strobe_data_class()
+picommon.spi_init( 0, 2, 30000 )
+strobe = PiStrobe( picommon.PORT_STROBE, 0.1 )
+#valid = strobe.set_enable( True )
 
 app = Flask( __name__ )
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    picommon.spi_init( 0, 2, 30000 )
-
-    strobe = PiStrobe( picommon.PORT_STROBE, 0.1 )
-    valid = strobe.set_enable( True )
-    
-    strobe_data = strobe_data_class()
-    strobe_data.hold = 0
-    
     if request.method == 'POST':
       if request.form['action'] == 'On':
         valid = strobe.set_hold( True )
@@ -24,7 +22,9 @@ def index():
       elif request.form['action'] == 'Off':
         valid = strobe.set_hold( False )
         strobe_data.hold = 0
+      return redirect( '/' )
     
+    strobe_data.num = strobe_data.num + 1
     return render_template( 'index.html', strobe=strobe_data )
 
 if __name__ == '__main__':
