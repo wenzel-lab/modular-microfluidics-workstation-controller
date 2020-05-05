71747722d5ac24bd3f0ae2e034443f2af16bf4dd
Christie Nel <christie.nel@gmail.com>
2020-05-05 19:54:29 +0100

    Continuous threaded updates working.

--- module-pi/pi_python/webapp/pi_webapp.py
+++ module-pi/pi_python/webapp/pi_webapp.py
@@ -1,6 +1,10 @@
+import time
+from threading import Thread
+from flask import Flask, flash, render_template, request, redirect
+from flask_socketio import SocketIO, join_room, emit, send
+import eventlet 
 import picommon
 from pistrobe import PiStrobe
-from flask import Flask, flash, render_template, request, redirect
 
 class strobe_data_class:
   hold = 0
@@ -11,22 +15,46 @@ picommon.spi_init( 0, 2, 30000 )
 strobe = PiStrobe( picommon.PORT_STROBE, 0.1 )
 #valid = strobe.set_enable( True )
 
+eventlet.monkey_patch()
 app = Flask( __name__ )
+socketio = SocketIO(app, async_mode = 'eventlet')
+thread = None
+
+def background_stuff():
+  while True:
+    time.sleep( 1 )
+    #t = str(time.perf_counter())
+    #socketio.emit('message', {'data': 'This is data', 'time': t})
+    socketio.emit( 'test', { 'hello': 'world' } )
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-      if request.form['action'] == 'On':
-        valid = strobe.set_hold( True )
-        strobe_data.hold = 1
-      elif request.form['action'] == 'Off':
-        valid = strobe.set_hold( False )
-        strobe_data.hold = 0
-      return redirect( '/' )
-    
-    strobe_data.num = strobe_data.num + 1
-    return render_template( 'index.html', strobe=strobe_data )
+  global thread
+  if thread is None:
+    thread = Thread( target=background_stuff )
+    thread.start()
+  
+  if request.method == 'POST':
+    if request.form['action'] == 'On':
+      valid = strobe.set_hold( True )
+      strobe_data.hold = 1
+    elif request.form['action'] == 'Off':
+      valid = strobe.set_hold( False )
+      strobe_data.hold = 0
+    return redirect( '/' )
+  
+  strobe_data.num = strobe_data.num + 1
+  return render_template( 'index.html', strobe=strobe_data )
+
+@socketio.on('create')
+def on_create(data):
+  socketio.emit( 'test' )
+
+@socketio.on('connect')
+def on_connect():
+  socketio.emit( 'test' )
 
 if __name__ == '__main__':
-    app.run( debug=True, host='0.0.0.0' )
-    
+#    app.run( debug=True, host='0.0.0.0' )
+  socketio.run( app, host='0.0.0.0', debug=True )
+  
